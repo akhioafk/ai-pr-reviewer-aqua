0f3c1596b6e7608da3aaac432ae1325cfd9a33ba
Akzhol Aidossuly <aidosulyakzhol@gmail.com>
2025-08-22 09:28:42 +0500

    updated tokens in limits.ts

--- src/limits.ts
+++ src/limits.ts
@@ -16,8 +16,8 @@ export class TokenLimits {
       this.maxTokens = 8000
       this.responseTokens = 2000
     } else {
-      this.maxTokens = 4000
-      this.responseTokens = 1000
+      this.maxTokens = 16300
+      this.responseTokens = 3000
     }
     // provide some margin for the request tokens
     this.requestTokens = this.maxTokens - this.responseTokens - 100
